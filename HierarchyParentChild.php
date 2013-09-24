157adf93ea4017b8ff3e2caff2514b605aaced9e
Sasha <shakal3351@gmail.com>
2013-09-24 20:30:30 +0300

    Test

---
+++ HierarchyParentChild.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Sasha
+ * Date: 24.09.13
+ * Time: 20:30
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace MyLib;
+
+
+class HierarchyParentChild {
+
+}
