161686e08f81ffa9c070d5726bf8d1db80678373
Shahelm <shakal3351@gmail.com>
2014-06-19 22:40:57 +0300

    Create base-config.php

---
+++ base-config.php
@@ -0,0 +1,91 @@
+<?php
+$config = array(
+    'm' => array(
+        'ptype' => array(
+            'filters' => array(
+                'universal-product' => array(
+                    0 => 9
+                ),
+                'min-price' => array(
+                    0 => 7
+                ),
+            ),
+            'buffers' => array(
+                'brand' => array(
+                    0 => 3
+                ),
+                'template' => array(
+                    0 => 2
+                ),
+                'model' => array(
+                    0 => 1
+                ),
+            ),
+        ),
+        'pstype' => array(
+            'filters' => array(
+                'universal-product' => array(
+                    0 => 9
+                ),
+                'min-price' => array(
+                    0 => 7
+                ),
+            ),
+            'buffers' => array(
+                'brand' => array(
+                    0 => 3
+                ),
+                'template' => array(
+                    0 => 2
+                ),
+                'model' => array(
+                    0 => 1
+                ),
+            ),
+        ),
+    ),
+    'mm' => array(
+        'ptype' => array(
+            'filters' => array(
+                'universal-product' => array(
+                    0 => 9
+                ),
+                'min-price' => array(
+                    0 => 7
+                ),
+            ),
+            'buffers' => array(
+                'brand'    => array(
+                    0 => 5
+                ),
+                'template' => array(
+                    0 => 4
+                ),
+                'category' => array(
+                    0 => 2
+                ),
+            ),
+        ),
+        'pstype' => array(
+            'filters' => array(
+                'universal-product' => array(
+                    0 => 9
+                ),
+                'min-price' => array(
+                    0 => 7
+                ),
+            ),
+            'buffers' => array(
+                'brand'    => array(
+                    0 => 5
+                ),
+                'template' => array(
+                    0 => 4
+                ),
+                'category' => array(
+                    0 => 2
+                ),
+            ),
+        ),
+    )
+);
